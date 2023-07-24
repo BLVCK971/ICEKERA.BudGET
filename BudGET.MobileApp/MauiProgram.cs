a5b8d24124231c4d6d23e3d9deaf28c72e610fba
Yoël Pépin <blackcrown971@gmail.com>
2023-07-25 00:00:43 +0200

    first UI implementation 1/2

--- BudGET.MobileApp/MauiProgram.cs
+++ BudGET.MobileApp/MauiProgram.cs
@@ -1,30 +1,46 @@
-﻿using BudGET.MobileApp.Data;
+﻿using Blazored.LocalStorage;
+using BudGET.MobileApp.Contracts;
+using BudGET.MobileApp.Data;
+using BudGET.MobileApp.Services;
 using Microsoft.Extensions.Logging;
 
-namespace BudGET.MobileApp
-{
-    public static class MauiProgram
-    {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
+namespace BudGET.MobileApp;
 
-            builder.Services.AddMauiBlazorWebView();
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        builder.Services.AddBlazoredLocalStorage();
+
 
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
-		builder.Logging.AddDebug();
+        builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();;
 #endif
 
-            builder.Services.AddSingleton<WeatherForecastService>();
+        builder.Services.AddSingleton(new HttpClient
+        {
+            BaseAddress = new Uri("https://localhost:7116")
+        });
+        builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://localhost:7116"));
 
-            return builder.Build();
-        }
+        builder.Services.AddSingleton<WeatherForecastService>();
+        builder.Services.AddScoped<IBudgetDataService, BudgetDataService>();
+        builder.Services.AddScoped<ICompteDataService, CompteDataService>();
+        builder.Services.AddScoped<IDepenseDataService, DepenseDataService>();
+        builder.Services.AddScoped<IObjectifDataService, ObjectifDataService>();
+        builder.Services.AddScoped<ISalaireDataService, SalaireDataService>();
+
+        return builder.Build();
     }
 }
