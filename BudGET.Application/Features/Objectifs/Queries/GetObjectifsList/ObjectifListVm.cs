efdde70cb8c8b95046493da4e03b7d5bc2a5fcd6
Yoël Pépin <blackcrown971@gmail.com>
2023-08-07 00:42:28 +0200

    Id dans le GetList

--- BudGET.Application/Features/Objectifs/Queries/GetObjectifsList/ObjectifListVm.cs
+++ BudGET.Application/Features/Objectifs/Queries/GetObjectifsList/ObjectifListVm.cs
@@ -1,6 +1,7 @@
 namespace BudGET.Application.Features.Objectifs.Queries.GetObjectifsList;
 public class ObjectifListVm
 {
+    public Guid Id { get; set; }
     public string Nom { get; set; } = string.Empty;
     public double Valeur { get; set; }
 }
--- BudGET.Application/Features/Salaires/Queries/GetSalairesList/SalaireListVm.cs
+++ BudGET.Application/Features/Salaires/Queries/GetSalairesList/SalaireListVm.cs
@@ -1,6 +1,7 @@
 namespace BudGET.Application.Features.Salaires.Queries.GetSalairesList;
 public class SalaireListVm
 {
+    public Guid Id { get; set; }
     public string Nom { get; set; } = string.Empty;
     public double Valeur { get; set; }
     public Guid CompteId { get; set; }
